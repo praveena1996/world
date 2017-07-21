cbdfd3e10083babae570e816ebe3710219780052
praveena1996 <praveenaselva20@gmail.com>
2017-07-21 17:47:54 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class hello
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=" ";
+for(int i=0;i<5;i++)(
+{
+System.out.print("Hello");
+}
+}
+}
